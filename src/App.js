e31df9ce184618748dc5b735ba087c55bdecd0d2
Ebi <kpemiogokimiebi@gmail.com>
2022-06-17 16:12:32 +0200

    Create React compnent & update Readme page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './Components/Header'
+
+export default function App(){
+    return (
+      <>
+      <Header />
+        <div className="container">
+          <h1>My App</h1>
+        </div>
+      </>
+    )
+}
---
+++ src/Index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import REACTDOM from 'react-dom/client'
+import App from "./App"
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
