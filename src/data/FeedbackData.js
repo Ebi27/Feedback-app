b38241974820953314293b55737e94c41f5a7904
Ebi <kpemiogokimiebi@gmail.com>
2022-06-25 23:34:06 +0200

    Create new component to display feedback data.

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,22 @@
+
+
+
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "lorem ipsum",
+  },
+  {
+    id: 2,
+    rating: 8,
+    text: "lorem ipsum",
+  },
+  {
+    id: 3,
+    rating: 4,
+    text: "lorem ipsum",
+  },
+];
+
+export default FeedbackData;
